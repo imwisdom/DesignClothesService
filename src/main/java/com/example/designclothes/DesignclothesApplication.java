68ccb64f9f6f9a0e877e5c9d8f5af9476cd4c73b
imwisdom <kjihye0340@naver.com>
2021-02-02 21:53:03 +0900

    initial commit

---
+++ src/main/java/com/example/designclothes/DesignclothesApplication.java
@@ -0,0 +1,13 @@
+package com.example.designclothes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignclothesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignclothesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/designclothes/DesignclothesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.designclothes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignclothesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
