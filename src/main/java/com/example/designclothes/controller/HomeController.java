f47b923bcf06dce1aa60b77fa306c2af3438be6f
imwisdom <kjihye0340@naver.com>
2021-02-17 19:05:51 +0900

    main page using Fashi website template

--- src/main/java/com/example/designclothes/controller/HomeController.java
+++ src/main/java/com/example/designclothes/controller/HomeController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping("/generic")
     public String home(Model model){
-        return "home";
+        return "generic";
     }
 }
