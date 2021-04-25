f327152b58e3f3ee49a1ac9e363760576fdfb944
imwisdom <kjihye0340@naver.com>
2021-04-25 14:30:28 +0900

    add order db

---
+++ src/main/java/com/example/designclothes/domain/Order.java
@@ -0,0 +1,42 @@
+package com.example.designclothes.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String userName;
+    private Long designId;
+    private boolean isChecked;
+
+    public Long getId(){
+        return this.id;
+    }
+    public void setId(Long id){
+        this.id = id;
+    }
+    public String getUserName(){
+        return this.userName;
+    }
+    public void setUserName(String userName){
+        this.userName = userName;
+    }
+    public Long getDesignId(){
+        return this.designId;
+    }
+    public void setDesignId(Long designId){
+        this.designId = designId;
+    }
+    public boolean getIsChecked(){
+        return this.isChecked;
+    }
+    public void setIsChecked(boolean isChecked){
+        this.isChecked = isChecked;
+    }
+
+}
